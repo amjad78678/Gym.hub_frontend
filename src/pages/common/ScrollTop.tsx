a3ae8a9976999d8a2a8645e943b9492c26d7fbc4
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2024-06-03 15:32:31 +0530

    scroll top changed

--- src/pages/common/ScrollTop.tsx
+++ src/pages/common/ScrollTop.tsx
@@ -1,15 +1,14 @@
-import  { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const ScrollTop = () => {
   const { pathname } = useLocation();
 
-
-    useEffect(() => {
-      if (!pathname.startsWith('/personal-trainer')) {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-      }
-    }, [pathname]);
+  useEffect(() => {
+    if (!pathname.startsWith("/profile")) {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  }, [pathname]);
 
   return null;
 };
