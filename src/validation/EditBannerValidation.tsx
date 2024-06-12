15e5445e78b40d16e8b6957349525ff3535b4b20
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2024-06-12 09:55:34 +0530

    after some changes

--- src/validation/EditBannerValidation.tsx
+++ src/validation/EditBannerValidation.tsx
@@ -3,10 +3,10 @@ import * as Yup from "yup";
 export const EditBannerValidation = Yup.object().shape({
   bannerImage: Yup.mixed()
     .nullable() 
-    .test("fileSize", "File size must be less than 2MB", (value) => {
+    .test("fileSize", "File size must be less than 4MB", (value) => {
       if (!value || typeof value === "string") return true; // Allow empty file or string
       if (value instanceof File) {
-        return value.size <= 3 * 1024 * 1024; // 3MB in bytes
+        return value.size <= 4 * 1024 * 1024; // 4MB in bytes
       }
       return false;
     })
