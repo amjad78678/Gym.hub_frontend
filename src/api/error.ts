d9d00f42ce2ac95ecdea9d2bd4a25424f85a8ceb
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2024-07-26 15:42:22 +0530

    bug fixed

--- src/api/error.ts
+++ src/api/error.ts
@@ -6,19 +6,19 @@ interface iErrorResponse {
 }
 const errorHandle = (error: Error | AxiosError) => {
   const axiosError = error as AxiosError;
-
   if (axiosError.response?.data) {
     const errorResponse = axiosError.response.data as iErrorResponse;
     if (errorResponse.message.includes("Not authorized")) {
       toast.error("Please login before proceeding");
-    } else if (errorResponse.message) {
-      toast.error(errorResponse.message);
+    } else if (errorResponse.message.includes("You are blocked by admin")) {
+      localStorage.removeItem("uLoggedIn");
+      localStorage.removeItem("userDetails");
+      window.location.href = window.location.href;
     } else {
-      
+      toast.error(errorResponse.message);
     }
   } else {
     toast.error("An error occurred. Please try again!");
-    
   }
 };
 
